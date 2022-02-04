4f5e28de029be88749af2c8c767ef3c2cfff7c36
Pat Barfield <patrickhbarfield@gmail.com>
2022-02-04 12:25:10 -0600

    another-fun-js

---
+++ js/external.js
@@ -0,0 +1,17 @@
+"use strict"
+
+console.log("Hello from external JavaScript")
+
+alert("Welcome to my website!");
+
+var userAnswer = prompt("What is your name?") {
+    return userAnswer;
+
+}
+
+/*
+prompt("tell me your favorite color");
+
+var userAnswer = prompt("tell me your favorite color") {
+    return userAnswer);
+}
---
+++ js/my-first-js-file.js
@@ -0,0 +1,32 @@
+"use strict";
+
+console.log('Hello from JavaScript!')
+
+    alert("We\n" +
+        "/*lcome to my Website!")
+console.log("This is fine");
+
+let myName = "Casey";
+
+console.log(myName);
+
+myName += " Edwards"
+
+console.log(myName);
+
+
+let myName = prompt(= 'Hi How are you?');
+
+myName += '' + prompt('last name?')
+
+console.log(myName.toUpperCase());
+
+if (confirm("Are you awesome today?")){
+    console.log("Always good to hear!");
+}
+
+
+let alerted = alert("hope u didn't console log this...");
+console.log(alerted);
+
+ /*
