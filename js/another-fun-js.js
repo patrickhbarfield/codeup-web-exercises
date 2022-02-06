4df04510e0c6279d7319ec5624ceed376fc5c317
Pat Barfield <patrickhbarfield@gmail.com>
2022-02-06 02:55:50 -0600

    fooling with git -a and -i options

--- js/another-fun-js.js
+++ js/another-fun-js.js
@@ -1,2 +1,20 @@
 "use strict";
 
+// */
+// works properly
+// *   -> Now, write a function named showName(username).
+// *       -> It takes in 'username' as an argument and alerts the user to their name
+// *       -> Pass an invocation of getName() as an argument to the invocation of showName()
+// *       -> showName() should be void function. That is, it should not return a value - only execute code
+// *   -> Test to ensure your code is working as expected
+
+
+
+// /*alert()function username()
+// /*let showName =
+
+function showName(username) {
+    alert(username);
+
+    showName(getName());
+}
