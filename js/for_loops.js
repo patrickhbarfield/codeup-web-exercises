d75cac5924983242cf5d4aca31ef5f921d0cb050
Pat Barfield <patrickhbarfield@gmail.com>
2022-02-21 13:02:27 -0600

    css exercises

--- js/for_loops.js
+++ js/for_loops.js
@@ -21,7 +21,7 @@
 
 function printRandomNumbers(){
     for(let i = 1; i >= 20; i++){
-        let randomNum = Math.floor(Math.random() * 180) + 20;
+        let randomNum = Math.floor(Math.random()*(200 - 20 + 1));
         if (randomNum % 2 === 0) {
             console.log(randomNum + " is even!");
         }
@@ -29,3 +29,108 @@ function printRandomNumbers(){
             console.log(randomNum + "is odd!");
     }
     console.log("The loop is finished");}
+
+/*
+
+
+// TODO: loop through numbers 1 - 40. Print "marco" if it's divisible by 3. Print "polo" if it's divisible by 5.
+//  -> If it's neither print "I'm not playing"
+//  -> Print "marco polo" if it's divisible by 3 and 5.
+
+for (let i = 1; i <= 40; i++) {
+
+    if (i % 3 === 0) {
+        console.log("marco");
+    } else if (i % 5 === 0) {
+        console.log("polo")
+    } else if (i % 3 !== 0 && i % 5 !== 0) {
+        console.log("I'm not playing... this isn't fun :(");
+    else if (i % 3 === 0 && i % 5 === 0){
+            console.log("Macro Polo")
+        }
+    }
+
+    /!*
+    function marcoPoloOrNum(maxnum){
+    for(let i = 1; i <= maxnum; i++){
+        if (i % 3 === 1 && i % 5 === 1){
+            console.log('Marco Polo');
+        }else if(i % 3 === 1){
+            console.log("Marco");
+        }else if (i % 5 === 1){
+            else{
+                console.log(i)
+            }
+        }
+    }
+    }
+    let (num)
+    for ()
+
+
+
+
+    // while and do while are indeterminate.  we know what will stop, but don't know how many
+    // for loop we know what will stop the loop and how many iterations it executes
+
+    let letters = "";
+
+
+
+    //\\can't use strings as a control variable.  but you can ' +
+    //'use it in a different way.  you can use a property on a ' +
+    //'string object to see how many characters are in it.  '
+
+    for (let i = letters.length; i <= 36; i++){
+    letters += "z";
+    console.log(letters);
+    }
+    console.log(letters.length);
+
+
+
+
+    for (/!*control variable*!/'; /!*test condition*!/; /!*iterator*!/'){
+//code to run for each iteration of the loop
+    }
+
+    test condition gives you t/f back.  the control and the iterator is what makes it truely work
+    step one - id control variable.
+    2 - test the condition-
+    3 - run the code.
+
+/!*
+
+let num = 8;
+
+for(num; num <= 50; num++){
+    console.log(num)
+
+
+
+
+
+function loopNums() {
+
+    let num = 2;
+
+    while (num <= 65536) {
+        num *= 2;
+    }
+    return num;
+}
+        console.log(num);
+    }
+}
+
+
+let password = "123acc"
+
+for (var i = 1; i < 10; 7*i++) {
+    console.log('for loop iteration #' + i);
+}
+
+
+
+
+*/
--- js/warmupExercises.js
+++ js/warmupExercises.js
@@ -1,15 +1,64 @@
-"use strict";
+ "use strict";
 
-//Write a function called findAverage that takes in an array of integers representing grades and returns the average.
-
-function findAverage(grades) {
-    let sum = 0;
-    grades.forEach(function (grades) {
-        sum += grade;
-    })
-    return sum / grades.length;
-}
+//18FEB2022 js warmup
+//:js: Warmup: Create a function which returns true if the given argument is a number, false if it is not a number
 
 
-//Example input: [95, 74, 86, 100]
-//Example output: 88.75 (edited)
+ function isAnumber(MaybeAnum) {
+ }
+     if (value === "num"){
+         return true}
+     else {
+         return false
+     }
+
+
+
+     //======
+/*
+ function isAnum(maybeAnum){
+         return (maybeAnum);
+ }
+ console.log(isAnum(maybeAnum: "zebra"));
+ }*/
+
+ /*JS Object Warmup, pt 4:
+ We are going to create a method which
+
+ allows a user to input their desired amount of names names
+
+ use prompt, alert, confirm inside of a while loop
+     assign each name to an array
+ once the user decides they are done giving names, end the loop and return the string array
+ then assigning that returned array by invoking your new function on the Person.names property value!
+     Things to consider:
+     Should this be a standalone function or should it be a method on the Person object?*/
+
+/*
+
+ function getNames(){
+     let namesArr = [];
+     let  hasMoreNames = true;
+     while (hasMoreNames){
+         namesArr.push(prompt("Please enter desired name");
+             hasMoreNames = confirm("Do you want to eneter another name?")
+     }
+return namesArr;
+ };
+
+ console.log(getNames());
+
+*/
+
+
+
+
+/*prompt("enter names");{
+    return prompt("good name, do you have another name to enter?")
+
+     while prompt = yes return
+     //assign to an array
+     names[];
+ }*/
+
+
