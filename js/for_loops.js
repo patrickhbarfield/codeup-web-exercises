23a16752814d47f9e7372b8b1d7bd2f3e40903ff
Pat Barfield <patrickhbarfield@gmail.com>
2022-02-21 22:47:20 -0600

    loops/css cleanup

--- js/for_loops.js
+++ js/for_loops.js
@@ -15,7 +15,7 @@
     showMultiplicationTable(10);
 
 // HELP with brackets/curly brace placement
-// HELP understanding randwom number generator
+// HELP understanding random number generator
 
 // #3 Generate 10 random numbers between 20-200 and output even or odd to the console
 
@@ -29,6 +29,60 @@ function printRandomNumbers(){
             console.log(randomNum + "is odd!");
     }
     console.log("The loop is finished");}
+
+
+// "use strict";
+//
+//
+// //Part Zero - Exercise 1: making a file named for_loops.js and link to loops.html
+// //DONE
+//
+// //Part One - Exercise 2
+// function showMultiplicationTable(num){
+//     for (var i = 1; i <= 10; i++){ //for (let multiplier = 1; multiplier <= 10; multiplier++)
+//         console.log(num + "multiplied by " + i + " = " + (num * i));
+//     }
+// }
+// showMultiplicationTable(7);
+//
+//
+// //Part Two - Exercise 3
+// function randomNumber(max, min) {
+//     return Math.floor(Math.random() * (max - min) + min);
+// }
+// for (var r = 1; i <= 10; r++) {
+//     let random = getRandomNum(200, 20);
+//     if (random % 2 === 0){
+//         console.log(random + " is even");
+//     } else {
+//         console.log(random + " is odd");
+//     }
+// }
+//
+// //Part Three - Exercise 4 create a for loop that uses console.log to create the output shown
+function treeOfNumbers(height) { // created a function
+    for (var t = 1; t <= height; t++){
+        // initialization of loop and condition
+        let tree = t += ''; //assign variable
+        // let space = '' + i; don't need this
+
+        for (var h = 1; h <= t; h++){
+            tree = i.toString();
+        }
+
+        //let gap = space.repeat( i - 1);
+        // tree = tree + gap; don't need this
+        console.log(tree);
+    }
+}
+treeOfNumbers(9);
+
+// Part Four - create a for loop that uses console.log to create the output shown
+// function minusFive() {
+//     for (let i = 1; i >= 5; i -= 5)
+// }
+
+
 
 /*
 
@@ -68,14 +122,10 @@ for (let i = 1; i <= 40; i++) {
     for ()
 
 
-
-
     // while and do while are indeterminate.  we know what will stop, but don't know how many
     // for loop we know what will stop the loop and how many iterations it executes
 
     let letters = "";
-
-
 
     //\\can't use strings as a control variable.  but you can ' +
     //'use it in a different way.  you can use a property on a ' +
@@ -86,9 +136,6 @@ for (let i = 1; i <= 40; i++) {
     console.log(letters);
     }
     console.log(letters.length);
-
-
-
 
     for (/!*control variable*!/'; /!*test condition*!/; /!*iterator*!/'){
 //code to run for each iteration of the loop
@@ -105,10 +152,6 @@ let num = 8;
 
 for(num; num <= 50; num++){
     console.log(num)
-
-
-
-
 
 function loopNums() {
 
@@ -129,8 +172,5 @@ let password = "123acc"
 for (var i = 1; i < 10; 7*i++) {
     console.log('for loop iteration #' + i);
 }
-
-
-
 
 */
