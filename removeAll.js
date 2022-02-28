ce945f37b5d39a2d481658a2d18037fd7d7b9e80
Pat Barfield <patrickhbarfield@gmail.com>
2022-02-28 09:33:40 -0600

    removeAll warmup

---
+++ removeAll.js
@@ -0,0 +1,19 @@
+
+var bikes = ["specialized","giant", "trek", "Impala"];
+console.log(removeAll(bikes, "Impala"));
+
+/*
+var bugs = ["mosquito", "ant", "scorpion", "ant", "ant",
+    "mosquito", "typo", "reference error", "type error"];
+
+console.log(removeAll(bugs, "ant"));
+
+// should print
+[
+    'mosquito',
+    'scorpion',
+    'mosquito',
+    'typo',
+    'reference error',
+    'type error'
+]*/
