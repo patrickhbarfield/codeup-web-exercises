e62743c382c32f3d88da863cfa52aec5d9bc289c
Pat Barfield <patrickhbarfield@gmail.com>
2022-03-11 12:34:53 -0600

    injecting values on the fly w/jQuery

--- js/my-custom-javascript.js
+++ js/my-custom-javascript.js
@@ -1,6 +1,30 @@
 "use strict";
 
+$("#myHeader").css());
+
+let student = {
+    userName: "Kamron",
+    id: 12,
+    role: student
+}
+
+$('body').append(getTemplate(student));
+/*
+INjecting values on the fly in Javascript.  When appended to the DOM, it's a whole ' +
+'chucnk on your dom with everything you need.  If you have bootstrap, you can have the class as a con' +
+'container '*/
+
+function getTemplate()[
+    return '<div>' +
+        '<h1>TEMPLATES >>>>yah</h1>
+        <p>Hey, ${userName}! u can use templaaaates!
+    </div>';
+
+                     ]
+
 $(document).ready(function() {
     alert("DOM has loaded" );
 });
 
+/*
+stop putting html in the html doc.  no more html hard-coding inside the body*/
