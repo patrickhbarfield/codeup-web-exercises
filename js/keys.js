060e758cbae59ecadeef207fe3c9733b30e18f65
Pat Barfield <patrickhbarfield@gmail.com>
2022-03-15 21:00:29 -0500

    API Work

---
+++ js/keys.js
@@ -0,0 +1,9 @@
+//will create variables here that will hold our API keys.  Store all your keys here.
+
+const OWM_KEY = '42c8ca86a5d2a16bc0ea5418cae4ec4d'
+const TWT_KEY = 'GkqWYKNitI0QzDBiU0dtV51b0'
+
+// set it to constant because it doesn't change.  Name them all caps, separated by underscores.
+
+//very important to git ignore your keys js. file.  prevents the sscrapers from  finding your exposed key.
+
