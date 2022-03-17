54eae97085406032e226a33fad8b59a90f8847ec
Pat Barfield <patrickhbarfield@gmail.com>
2022-03-17 15:15:24 -0500

    geocode lec

--- js/keys.js
+++ js/keys.js
@@ -1,8 +1,8 @@
 //will create variables here that will hold our API keys.  Store all your keys here.
 
-const OWM_KEY = '42c8ca86a5d2a16bc0ea5418cae4ec4d'
+const OWM_KEY = '42c8ca86a5d2a16bc0ea5418cae4ec4d';
 const TWT_KEY = 'AAAAAAAAAAAAAAAAAAAAANccaQEAAAAA7WbeYPEsiY2fvcxeaZA%2BnEbAzas%3DpQbobwxwmKQJsH2X0uV9FCfOUgdQKZxAY9Yyd4Pj37nrF6r0sH'
-
+const MPBX_KEY = 'pk.eyJ1IjoicGF0LWIiLCJhIjoiY2wwdmQ5dTE2MGk0dDNjbXR0YTA2amV0bSJ9.h8LMqMpVHaRPdA942qkqFg';
 // set it to constant because it doesn't change.  Name them all caps, separated by underscores.
 
 //very important to git ignore your keys js. file.  prevents the sscrapers from  finding your exposed key.
