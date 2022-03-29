a5e477368bb21d6cba19cf2531fa92d23f20dbd7
Pat Barfield <patrickhbarfield@gmail.com>
2022-03-29 13:12:53 -0500

    Promises

---
+++ js/promises.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const MessageUrl = 'https://api.github.com/users/patrickhbarfield/events';
+
+const dateUrl = 'https://api.github.com/repos/patrickhbarfield/codeup-web-exercises/commits';
+
+const date = fetch('dateURL', {headers: {'Authorization': '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'}});
+
+
